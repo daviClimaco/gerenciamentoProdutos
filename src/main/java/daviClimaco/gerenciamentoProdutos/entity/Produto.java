c290df0f3fdfee8ea5d0f21762e61fd0acc30715
daviClimaco <davibomfimclimaco@gmail.com>
2026-03-22 20:30:02 -0300

    Criado getters e setters e adicionado service para produto.

--- src/main/java/daviClimaco/gerenciamentoProdutos/entity/Produto.java
+++ src/main/java/daviClimaco/gerenciamentoProdutos/entity/Produto.java
@@ -22,5 +22,35 @@ public class Produto {
     @Column(name = "qtdEstoque")
     private Integer estoque;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
 }
---
+++ src/main/java/daviClimaco/gerenciamentoProdutos/services/ProdutoService.java
@@ -0,0 +1,34 @@
+package daviClimaco.gerenciamentoProdutos.services;
+
+import daviClimaco.gerenciamentoProdutos.entity.Produto;
+import daviClimaco.gerenciamentoProdutos.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+
+    private ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public void salvar(Produto produto){
+        produtoRepository.save(produto);
+    }
+
+    public void deletar(Long id){//deleta pelo id comoparametro
+        produtoRepository.deleteById(id);
+    }
+
+    public Produto buscarProdutoPirId(Long id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public List<Produto> buscarTodos(){
+        return produtoRepository.findAll();
+    }
+
+}
